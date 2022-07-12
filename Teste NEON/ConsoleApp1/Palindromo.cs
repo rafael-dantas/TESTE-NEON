2019df73515ccc5efb140fb0e237b2f145f0113f
Rafael Dantas <53061135+rafael-dantas@users.noreply.github.com>
2022-07-12 18:59:50 -0300

    Add files via upload

---
+++ Teste NEON/ConsoleApp1/Palindromo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Palindromo
+    {
+        public bool VerificaPalindromo()
+        {
+            string texto = "O romano acata amores a damas amadas e Roma ataca o namoro";
+
+            texto = texto.ToLower().Replace(" ", "");
+
+            char[] str = texto.ToCharArray();
+
+            int qtd = str.Length;
+
+            string strComparacao = "";
+
+            for (int i = (qtd - 1); i >= 0; i--)
+            {
+                strComparacao += str[i].ToString();
+            }
+
+            if (texto == strComparacao)
+                return true;
+            else
+                return false;
+        }
+
+
+        public void GetFibonacci(int numero)
+        {
+            int controle = 1;
+            int anterior = 0;
+            int auxiliar = 0;
+
+            for (int i = 0; i < numero; i++)
+            {
+                auxiliar = anterior;
+                anterior = controle;
+                controle = anterior + auxiliar;
+                Console.WriteLine(controle);
+            }
+        }
+    }
+}
---
+++ Teste NEON/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
+Console.WriteLine("Hello, World!");
+
+Palindromo p = new Palindromo();
+
+Console.WriteLine(p.VerificaPalindromo() ? "SIM" : "NÂO");
+
+p.GetFibonacci(6);
+
+Console.ReadKey();
